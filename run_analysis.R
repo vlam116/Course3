e4c25bc60961eda7d12b3f5be4c91b396e1c153f
vlam116 <vlam116@gmail.com>
2020-04-04 14:50:37 -0700

    added comments, factorized activity labels, reformatted code

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,22 @@
+library(tidyverse)
+library(plyr)
+library(stringr)
+
+
+## Reading in the training and testing sets and the activity labels, then converting to tibble format for
+## better readability. 
+
 train = read.table("X_train.txt")
 test = read.table("X_test.txt")
 train_y = read.table("y_train.txt")
 test_y = read.table("y_test.txt")
-train = as.tibble(train)
+train = as_tibble(train)
 test = as_tibble(test)
 train_y = as_tibble(train_y)
 test_y = as_tibble(test_y)
+
+## Doing the same for each of the estimated variables from the 3 raw signals. 
+
 bax_test = read.table("body_acc_x_test.txt")
 bax_train = read.table("body_acc_x_train.txt")
 bay_test = read.table("body_acc_y_test.txt")
@@ -16,6 +27,8 @@ bgx_test = read.table("body_gyro_x_test.txt")
 bgx_train = read.table("body_gyro_x_train.txt")
 bgy_test = read.table("body_gyro_y_test.txt")
 bgy_train = read.table("body_gyro_y_train.txt")
+bgz_train = read.table("body_gyro_z_train.txt")
+bgz_test = read.table("body_gyro_z_test.txt")
 tax_test = read.table("total_acc_x_test.txt")
 tax_train = read.table("total_acc_x_train.txt")
 tay_train = read.table("total_acc_y_train.txt")
@@ -26,7 +39,7 @@ bax_test = as.tibble(bax_test)
 bax_test = as_tibble(bax_test)
 bax_train = as_tibble(bax_train)
 bay_test = as_tibble(bax_test)
-bay_train=as_tibble(bay_train)
+bay_train = as_tibble(bay_train)
 baz_test = as_tibble(baz_test)
 baz_train = as_tibble(baz_train)
 bgx_test = as_tibble(bgx_test)
@@ -42,22 +55,26 @@ tay_train = as_tibble(tay_train)
 tay_test = as_tibble(tay_test)
 taz_test = as_tibble(taz_test)
 taz_train = as_tibble(taz_train)
+
+## Now reading in the final txt files containing the numeric label for each of the 30 participants. 
+
 subject_test = read.table("subject_test.txt")
 subject_train = read.table("subject_train.txt")
 subject_test = as_tibble(subject_test)
 subject_train = as_tibble(subject_train)
 
-bax_bind = bind_rows(bax_train, bax_test)
-bay_bind = bind_rows(bay_train, bay_test)
-baz_bind = bind_rows(baz_train, baz_test)
+## Combining the rows from the 561 features in the training and testing sets. This will be the "main"
+## dataframe that I will gradually bind other cleaned features and labels to. 
+## The features table contains duplicate names that need to be replaced. From the additional
+## info texts and README, I was able to deduce the names of these features. The features being duplicated
+## came from the bandsEnergy() variables estimated from the raw signals. Measurements along the X,Y, and
+## Z-axes are very common throughout many of the data frames, so I appended the bandsEnergy variables with
+## the XYZ labels to remedy the duplication issue. Then, I was able to reformat the features file properly
+## by pivoting the rows to become the columns, then replace the missing column names in the combined train
+## /test set with the variable names. 
 
-bgx_bind = bind_rows(bgx_train, bgx_test)
-bgy_bind = bind_rows(bgy_train, bgy_test)
-bgz_bind = bind_rows(bgz_train, bgz_test)
-
-tax_bind = bind_rows(tax_train, tax_test)
-tay_bind = bind_rows(tay_train, tay_test)
-taz_bind = bind_rows(taz_train, taz_test)
+x_bind = bind_rows(train, test)
+y_bind = bind_rows(train_y, test_y)
 
 features = read.table("features.txt")
 features = as_tibble(features)
@@ -68,20 +85,46 @@ features$V2[c(317:330,396:409,475:488)] = str_replace(features$V2[c(317:330,396:
 features$V2[c(331:344,410:423,489:502)] = str_replace(features$V2[c(331:344,410:423,489:502)], "f","Zf")
 features = features %>% pivot_wider(names_from = V2, values_from = V1)
 
-replacement = c(features$V2)
+replacement = c(names(features))
 
 for(i in 1:length(replacement)) {
-  names(all)[i] = replacement[i]
+  names(x_bind)[i] = replacement[i]
 }
 
-y_all = bind_rows(train_y, test_y)
-
-all_bind = bind_rows(train_bind, test_bind)
-all = bind_cols(all_bind,y_all)
+y_bind = y_bind %>% rename(Activity = V1)
+y_bind$Activity = as.factor(y_bind$Activity)
+levels(y_bind$Activity) = mapvalues(y_bind$Activity, 
+                                from = c(1,2,3,4,5,6), 
+                                to = c("WALKING", "WALKING_UPSTAIRS", "WALKING_DOWNSTAIRS", 
+                                       "SITTING", "STANDING", "LAYING"))
 
 subject_train = subject_train %>% rename(ID = V1)
 subject_test = subject_test %>% rename(ID = V1)
 subject_all = bind_rows(subject_train, subject_test)
 
-all = bind_cols(y_all,all)
-all = bind_cols(subject_all,all)
+xy_bind = bind_cols(y_bind, x_bind)
+id_xy_bind = bind_cols(subject_all, xy_bind)
+
+## The rest of the txt files each contain 128 readings from the "accelerometer and gyroscope 3-axial raw
+## signals" (tAcc-XYZ, tGyro-XYZ) and the estimated body acceleration described in the README, which we
+## can call eBodyAcc-XYZ for a total of 9 different raw variables which were used to derive the above
+## 561 features. These dataframes are missing column names but are otherwise rather tidy. All that needs
+## to be done is to add proper column names and append them to the main dataframe. 
+
+## First, dealing with the estimated body acceleration readings. We can simply replace the column names
+## with some variation of eBodyAcc(XYZ)Reading(1:128). 
+
+bax_bind = bind_rows(bax_train, bax_test)
+bay_bind = bind_rows(bay_train, bay_test)
+baz_bind = bind_rows(baz_train, baz_test)
+
+
+
+bgx_bind = bind_rows(bgx_train, bgx_test)
+bgy_bind = bind_rows(bgy_train, bgy_test)
+bgz_bind = bind_rows(bgz_train, bgz_test)
+
+tax_bind = bind_rows(tax_train, tax_test)
+tay_bind = bind_rows(tay_train, tay_test)
+taz_bind = bind_rows(taz_train, taz_test)
+
